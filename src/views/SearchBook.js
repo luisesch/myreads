01cbe107415e3bb84a20dec4bea7e2d4e6dacc0e
Luise Schneider <luise.schneider@berlin.de>
2021-12-10 16:51:16 +0100

    implement search, showing number of results and cllear button

--- src/views/SearchBook.js
+++ src/views/SearchBook.js
@@ -8,11 +8,49 @@ class SearchBook extends Component {
     books: PropTypes.array.isRequired,
   };
 
-  render() {
-    const { books } = this.props;
-    console.log(books);
+  state = {
+    query: "",
+  };
 
-    return <ListBooks books={books} />;
+  updateQuery = (query) => {
+    this.setState(() => ({
+      query: query,
+    }));
+  };
+
+  clearQuery = () => this.updateQuery("");
+
+  render() {
+    const { query } = this.state;
+    const { books } = this.props;
+
+    const filteredBooks =
+      query === ""
+        ? books
+        : books.filter((book) =>
+            book.title.toLowerCase().includes(query.toLowerCase())
+          );
+
+    return (
+      <div>
+        <input
+          type="text"
+          placeholder="Search books"
+          value={this.state.query}
+          onChange={(event) => this.updateQuery(event.target.value)}
+          className="input-search"
+        />
+        {filteredBooks.length !== books.length && (
+          <div>
+            <span>
+              Now showing {filteredBooks.length} of {books.length}
+            </span>
+            <button onClick={this.clearQuery}>Show all</button>
+          </div>
+        )}
+        <ListBooks books={filteredBooks} />;
+      </div>
+    );
   }
 }
 
